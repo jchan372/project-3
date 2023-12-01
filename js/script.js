da27985e3f4785fd785177460503144b1ea2b282
jchan372 <143012077+jchan372@users.noreply.github.com>
2023-12-01 08:47:12 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
+var map;
+function initMap() {
 
-<script>
-    var map;
-    function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.8722, lng: 87.6188},
+    zoom: 8
+  });
+}
 
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 41.8722, lng: 87.6188},
-      zoom: 8
-    });
-  }
-</script>
+window.initMap = initMap;
